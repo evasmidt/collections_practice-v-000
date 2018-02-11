b08e318b4e80645e231ea45caca4606b52bcc09c
Eva Smidt <shevangeline@gmail.com>
2018-02-11 17:06:03 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,7 @@
 
+#Build a method sort_array_asc that takes in an 
+#array of integers and returns a copy of the 
+#array with the integers in ascending order.
+def sort_array_asc(integer_array)
+  integer_array.sort
+end
