4c252958045363d781e426845125d9749c6b13b3
Eva Smidt <shevangeline@gmail.com>
2018-02-11 17:07:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,12 @@
 def sort_array_asc(integer_array)
   integer_array.sort
 end
+
+#Build a method sort_array_desc that takes 
+#in an array of integers and returns a copy of the 
+#array with the integers in descending order. 
+#Remember that .sort takes a block in which you 
+#can specify how you want your array sorted.
+def sort_array_desc 
+
+end
